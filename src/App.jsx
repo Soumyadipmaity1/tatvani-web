6f80043b45748c49f82201068179a8bd746319b4
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-14 12:44:25 +0530

    Add routing and navigation to App component

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+// import Careers from './pages/careers';
+import Stories from "./pages/stories"
+import Contact from './pages/contact';
+import Poetry from './pages/poetry';
+import News from './pages/news';
+import Navbar from './componets/Essestials/navbar';
+import Footer from './componets/Essestials/Footer';
+import Submission from './pages/submission';
+
+function App() {
+  return (
+    <Router>
+
+<Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/poetry" element={<Poetry />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/submission" element={<Submission />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/stories" element={<Stories/>} />
+        {/* <Route path="/careers" element={<Careers />} /> */}
+      </Routes>
+<Footer/>
+    </Router>
+  );
+}
+
+export default App;
