87a86cfcebc390705c94c902563386007b762a79
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-15 02:08:17 +0530

    Add AboutSection and FeaturesSection components to the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,13 @@
+import AboutSection from "../components/HomePage/AboutSection";
+import FeaturesSection from "../components/HomePage/Feature";
 import HomeSlider from "../components/HomePage/HomeSlider";
 
 export default function home() {
   return (
     <div>
       <HomeSlider/>
-
+<FeaturesSection/>
+<AboutSection/>
     </div>
   )
 }
