61a08b6614224edd2641ec515441ebf2127771d9
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-16 12:46:11 +0530

    Add missing components to the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,10 @@
 import AboutSection from "../components/HomePage/AboutSection";
+import AdvertisementSection from "../components/HomePage/AdvertisementSection";
 import FeaturesSection from "../components/HomePage/Feature";
 import HomeSlider from "../components/HomePage/HomeSlider";
+import LatestNews from "../components/HomePage/LatestNews";
+import FeaturedPoem from "../components/HomePage/LatestPoem";
+import FeaturedStories from "../components/HomePage/LatestStories";
 
 export default function home() {
   return (
@@ -8,6 +12,10 @@ export default function home() {
       <HomeSlider/>
 <FeaturesSection/>
 <AboutSection/>
+<FeaturedPoem/>
+<FeaturedStories/>
+<LatestNews/>
+<AdvertisementSection/>
     </div>
   )
 }
