d81803250a6c249b3ea993116a4129289bc63861
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-18 02:02:20 +0530

    Refactor App.jsx to remove commented out code for Careers page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
-// import Careers from './pages/careers';
 import Stories from "./pages/stories"
 import Contact from './pages/contact';
 import Poetry from './pages/poetry';
