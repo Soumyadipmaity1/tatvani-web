8de8143521c44201b9d5cb86449a195009af822f
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-18 02:39:52 +0530

    Refactor contact page layout and remove unnecessary code

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,7 +3,6 @@ import ContactForm from '../components/ContactPage/ContactForm'
 export default function contact() {
   return (
     <div>
-      {/* //contact us frontpage */}
       <img src=" https://t3.ftcdn.net/jpg/05/30/96/04/360_F_530960431_c8fPd3HansYvrSJ4fJxZqp9OhjQmYoll.jpg" alt="" className="w-full  object-cover object-center"/>
       <ContactForm/>
     </div>
