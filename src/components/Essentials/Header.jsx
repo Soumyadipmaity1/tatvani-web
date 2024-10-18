21dbc74ca933952a1e0c26a01f1179c2437bc93a
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-18 19:24:27 +0530

    Refactor Header component to add logout functionality

---
+++ src/components/Essentials/Header.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+const Header = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+      // Perform any logout logic here (e.g., clear tokens, etc.)
+      // Redirect to the login page
+      navigate('/login');
+    };
+  return (
+    <header className="bg-gray-950 text-white p-4 fixed top-0 left-64 right-0 z-10">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl">Admin Dashboard</h1>
+        <button onClick={handleLogout} className="bg-red-600 text-white p-2 rounded-md hover:bg-red-700"> Logout</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
