69af2848007656ad1d5207b24fc7c5a1c1e1e445
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-18 23:43:24 +0530

    Refactor background color and spacing in AboutSection component

--- src/components/HomePage/AboutSection.jsx
+++ src/components/HomePage/AboutSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const AboutSection = () => {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-evenly px-6 py-12 bg-white space-y-8 lg:space-y-0 lg:space-x-16">
+    <div className="flex flex-col lg:flex-row items-center justify-evenly px-6 py-20 bg-white space-y-8 lg:space-y-0 lg:space-x-16">
 
       <motion.div
         className="text-left max-w-lg"
@@ -11,17 +11,17 @@ const AboutSection = () => {
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.8 }}
       >
-        <h1 className="text-4xl font-bold mb-4">
+        <h1 className="text-4xl text-[#3d3b3a] font-bold mb-4">
           Driving Success, One Client At A Time
         </h1>
-        <p className="text-gray-500 mb-6">
+        <p className="text-gray-700 mb-8">
           Our commitment to excellence has led to remarkable achievements. Here
           are the values that reflect our dedication and the trust of our
           clients. We work tirelessly to ensure your success and satisfaction.    Our commitment to excellence has led to remarkable achievements. Here
           are the values that reflect our dedication and the trust of our
           clients. We work tirelessly to ensure your success and satisfaction.
         </p>
-        <Link to="/about" className="bg-[#32658e] text-white px-5 py-2 rounded">
+        <Link to="/about" className="bg-[#ff7f57] text-[#3d3b3a] font-bold px-6 py-3 mt-4 hover:bg-white hover:text-orange-600 border-2 shadow-lg border-orange-600 rounded">
           More About Us
         </Link>
       </motion.div>
@@ -35,7 +35,7 @@ const AboutSection = () => {
         <img
           src="https://via.placeholder.com/400x300"
           alt="Placeholder"
-          className="rounded-lg shadow-lg border border-[#32658e]"
+          className="rounded-lg shadow-lg "
         />
       </motion.div>
     </div>
