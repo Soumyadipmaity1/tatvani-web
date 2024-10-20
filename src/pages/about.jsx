24761de0139ddee3e670a8cea7287efba0443e33
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-20 15:17:24 +0530

    feat: Improved layout and content structure of About page by adding a new section and rearranging elements.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,9 +7,9 @@ import AdvertisementSection from "../components/HomePage/AdvertisementSection";
 export default function About() {
   return (
     <div className="bg-gradient-to-b from-yellow-50 via-blue-50 to-yellow-50 dark:from-gray-900 dark:via-gray-900 dark:to-gray-800 min-h-screen">
-      <div className="max-w-7xl mx-auto px-4  sm:px-6 lg:px-8 py-16 space-y-32">
+      <div className="max-w-7xl mx-auto px-2  sm:px-6 lg:px-8 sm:py-16 sm:space-y-32">
         <AboutUsSection />
-        <div className="grid md:grid-cols-2 gap-12">
+        <div className="grid md:grid-cols-2  gap-12">
           <MissionSection />
           <VisionSection />
         </div>
