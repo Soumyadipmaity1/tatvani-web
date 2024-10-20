a85447ffbee39ee59dbe26c26c8530454d2bf07c
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-20 15:01:47 +0530

    feat: Added sections for mission, vision and team information to the About page.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,21 @@
 import AboutUsSection from "../components/AboutPage/AboutSection";
+import MissionSection from "../components/AboutPage/MissionSection";
+import VisionSection from "../components/AboutPage/VisionSection";
+import TeamSection from "../components/AboutPage/TeamSection";
 import AdvertisementSection from "../components/HomePage/AdvertisementSection";
 
 export default function About() {
   return (
-    <div>
-      <AboutUsSection/>
-      <AdvertisementSection/>
+    <div className="bg-gradient-to-b from-yellow-50 via-blue-50 to-yellow-50 dark:from-gray-900 dark:via-gray-900 dark:to-gray-800 min-h-screen">
+      <div className="max-w-7xl mx-auto px-4  sm:px-6 lg:px-8 py-16 space-y-32">
+        <AboutUsSection />
+        <div className="grid md:grid-cols-2 gap-12">
+          <MissionSection />
+          <VisionSection />
+        </div>
+        <TeamSection />
+      </div>
+      <AdvertisementSection />
     </div>
   )
 }
