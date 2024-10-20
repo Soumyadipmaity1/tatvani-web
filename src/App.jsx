fc5ba953c0c453d937ba039a048716cd562721aa
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-20 13:40:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Stories from './pages/stories';
